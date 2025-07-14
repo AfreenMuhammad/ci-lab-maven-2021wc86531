bc3a69449ced3f90267055995bea3188b7f50d96
AfreenMuhammad <2021wc86531@wilp.bits-pilani.ac.in>
2025-07-14 20:31:51 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 public class App {
     public static void main(String[] args) { 
        System.out.println("🚀 CI Pipeline Demo"); 
-       System.out.println("Hello from GitHub, Maven, and Jenkins!"); 
+       System.out.println("CI triggered by GitHub update ✅"); 
        System.out.println("Build triggered at: " + LocalDateTime.now()); 
    }
 }
